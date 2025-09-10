4420a35fcedc8af72816266cddf15c363a08c769
codesnas <67037511+codesnas@users.noreply.github.com>
2025-09-10 14:07:06 +0800

    Adjust s9xxx

--- router/immortalwrt/diy-part1.sh
+++ router/immortalwrt/diy-part1.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 #========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt for Amlogic S9xxx STB
+# https://github.com/codesnas/openwrt
+# Description: Automatically Build OpenWrt
 # Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/coolsnowwolf/lede / Branch: master
+# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
 #========================================================================================================================
-
-# Uncomment a feed source
-# sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
-# sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
 
 # Add a feed source
 # sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
 # other
-# rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
+# rm -rf package/emortal/{autosamba,ipv6-helper}
 
